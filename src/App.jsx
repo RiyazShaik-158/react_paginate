e4a3d697a87a990cea0054482ffd681e433704b8
Reyaz <riyaz.shaik.1481@gmail.com>
2023-09-18 00:04:45 +0530

    tryingReact+Vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.scss';
+
+function App() {
+  return (
+    <div className='App_main'>
+      <h4>Hello World</h4>
+    </div>
+  )
+}
+
+export default App
